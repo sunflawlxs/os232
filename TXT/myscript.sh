689c2e33713c01fc8c81e19e4da95cf2483415f8
sunflawlxs <123561471+sunflawlxs@users.noreply.github.com>
2023-11-27 10:39:32 +0700

    WEEK09

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -16,7 +16,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 # ####################### Replace REC2 ####
-REC2="69281B9E46FF1911"
+REC2="C6AD3BC1695641A4"
 # ####################### ####### #### ####
 # REC1: public key
 REC1="63FB12B215403B20"
